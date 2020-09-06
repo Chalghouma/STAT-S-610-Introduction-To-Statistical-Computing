a0b2598de41c5848e2378204f8bb6bc296d7a2b9
Cherif Redissi <cherifredissi@hotmail.com>
2020-09-05 23:24:39 -0400

    feat: HW1 store word/character count & generate unique honorific names

--- hw1.R
+++ hw1.R
@@ -18,10 +18,33 @@ for (book_file_name in book_file_names) {
 
   #We get the title
   title_regex = "Title:( [A-z]+)+"
-  
+
   title <- regmatches(book_text, regexpr(title_regex, book_text))
-  #We remove the "Title: " 
-  frame[index, 1] <- substring(title,8)
+  #We remove the "Title: " from the matched value 
+  frame[index, 1] <- substring(title, 8)
+
+  #We get the word count
+  splitted <- strsplit(book_text, " ")
+  splitted
+  frame[index, 2] <- length(splitted[[1]])
+
+  #We get the characters' count
+  frame[index, 3] <- nchar(book_text)
+
+  #We get the individuals' names (as honorifics)
+  #The Mr. Mrs. Dr. Ms. Miss. is dealt with (Mr)|(Mrs)|(Dr)|(Ms)|(Miss))\.
+  #For the " E.", " S.", we use ( [A-Z]\.) that we repeat several 0-N times (the name doesn't necessairly have that)
+  #As for the normal first/middle/last names, we can be sure that even if placed in the middle of a sentence
+  #there's no way the word that comes after starts with a capital letter. which is why we're using the [A-Z] constraint right after the space
+  #To get a chunk of the name we use this: ( [A-Z]\.)*( [A-Z][a-z]*) that we repeat at least once.
+  #Lastly, we use the \\. instead of \. since we're defining it inside a string
+  individual_honorific_regex <- "((Mr)|(Mrs)|(Dr)|(Ms)|(Miss))\\.( [A-Z]\\.)*( [A-Z][a-z]*)+"
+  names <- regmatches(book_text, gregexpr(individual_honorific_regex, book_text))
+  
+  #We print & store the unique names' info
+  unique_names <-unique(names[[1]])
+  unique_names
+  frame[index,4] <- length(unique_names)
   index <- index + 1
 }
 
