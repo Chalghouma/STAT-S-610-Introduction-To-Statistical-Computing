5e139f45e7ee941c790607524d73b3ea66c5e515
Cherif Redissi <cherifredissi@hotmail.com>
2020-10-01 16:17:00 -0400

    feat: HW3 Bills number of Yea votes

--- HW3/hw3.r
+++ HW3/hw3.r
@@ -1,6 +1,7 @@
 #install.packages('reshape2')
 library(readr)
 library(reshape2)
+library(dplyr)
 
 bills_initial_df = read_csv('HW3/bills.csv')
 votes_initial_df = read_csv('HW3/votes.csv')
@@ -41,5 +42,41 @@ append_measure_activity = function(members_df, bills_df) {
   measure_of_activity = occurences_of_members_in_bills(bills_subjects, last_names)
   return(cbind(members_df, measure_of_activity))
 }
+#head(append_measure_activity(members_initial_df,bills_initial_df))
 
-head(append_measure_activity(members_initial_df,bills_initial_df))
+
+#************************************************
+#Question 3) 
+#************************************************
+get_number_of_Yea = function(vote_vector) {
+  count = 0
+  for (x in 1:length(vote_vector)) {
+    if (!is.na(vote_vector[x]) && vote_vector[x] == "Yea")
+      count = count + 1
+  }
+
+  return(count)
+
+  #Shorter but includes NA
+  yea = vote_vector[vote_vector == "Yea"]
+  return(length(yea))
+}
+
+get_bill_ids = function(votes_df) {
+  return(colnames(votes_df)[-1])
+}
+
+get_bills_number_of_Yea_df = function(votes_df) {
+  bill_ids = get_bill_ids(votes_df)
+
+  numbers = c()
+  for (index in 1:length(bill_ids)) {
+    vote_vector = votes_df[bill_ids[index]][[1]]
+    # vote_vector = votes_df[,index+1][[1]]
+    number_of_yea = get_number_of_Yea(vote_vector)
+    numbers = append(numbers, number_of_yea)
+  }
+  return(data.frame(bill_ids,numbers))
+}
+
+head(get_bills_number_of_Yea_df(votes_initial_df))
