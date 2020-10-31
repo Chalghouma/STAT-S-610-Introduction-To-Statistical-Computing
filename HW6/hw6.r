6c05793cd6c7a5ea6e3e2ae606eac0270d5e9c3e
Cherif Redissi <cherifredissi@hotmail.com>
2020-10-31 17:40:24 -0400

    feat: HW6 report MLE results

--- HW6/hw6.r
+++ HW6/hw6.r
@@ -7,7 +7,7 @@ log_likelihood_function = function(X , theta){
 
 derivative_log_likelihood = function(X,theta){
     f <- function(xi,theta){
-        return (2*(xi-theta) / (1 + (x-theta)^2))
+        return (2*(xi-theta) / (1 + (xi-theta)^2))
     }
     return (sum(sapply(X, FUN=f,  theta)))
 }
@@ -19,4 +19,27 @@ second_derivative_log_likelihood = function(X,theta){
     return (sum(sapply(X,FUN=f, theta)))
 }
 
-second_derivative_log_likelihood(1:10,0.1)
+maximum_likelihood_estimate = function(X, starting_theta, criterion){
+    should_stop_searching = function(X, current_theta, criterion){
+        return (derivative_log_likelihood(X,current_theta) < criterion)
+    }
+
+    current_theta = starting_theta
+    while(!should_stop_searching(X,current_theta,criterion)){
+        current_theta = current_theta - derivative_log_likelihood(X,current_theta) / second_derivative_log_likelihood(X,current_theta)
+    }
+
+    return (current_theta)
+}
+
+report_mle_results = function(X, starting_thetas , criterion){
+    for(i in 1:length(starting_thetas)){
+        mle = maximum_likelihood_estimate(X,starting_thetas[i] , criterion)
+        print(paste('MLE on Theta = ',starting_thetas[i], ' = ', mle ))
+    }
+}
+
+
+data = c(−2.09 , −2.68, −1.92, −1.76, −2.12, 2.21, 1.97, 1.61, 1.99, 2.18)
+thetas = c(-2,-1,0,1,2)
+report_mle_results( data , thetas , 0.1 )
