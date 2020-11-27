949611bf0d3515068e06e89612f5d9f5ec49454d
Cherif Redissi <cherifredissi@hotmail.com>
2020-11-27 18:15:42 -0500

    test: [Project] test outward calculation on dummy sample

--- Project/selector.r
+++ Project/selector.r
@@ -2,26 +2,26 @@
 library(readr)
 library(tidyverse)
 
-cases = read_csv('Project/data/judicial.csv')
-my_data <- as_tibble(cases)
-ids = my_data[, 1][[1]]
-cases_in_range = function(df, from, to) {
-  ids_and_years = my_data %>% select(1, 4)
-  return(ids_and_years %>% filter(year > 2754))
-}
+# cases = read_csv('Project/data/judicial.csv')
+# my_data <- as_tibble(cases)
+# ids = my_data[, 1][[1]]
+# cases_in_range = function(df, from, to) {
+#   ids_and_years = my_data %>% select(1, 4)
+#   return(ids_and_years %>% filter(year > 2754))
+# }
 
-process_line = function(line, inward_vector, outward_vector) {
-  splitted = str_split(line, ' ')[[1]]
-  from = splitted[1] %>% strtoi()
-  to = splitted[2] %>% strtoi()
-  inward_vector[to] = inward_vector[to] + 1
-  outward_vector[from] = outward_vector[from] + 1
-}
+# process_line = function(line, inward_vector, outward_vector) {
+#   splitted = str_split(line, ' ')[[1]]
+#   from = splitted[1] %>% strtoi()
+#   to = splitted[2] %>% strtoi()
+#   inward_vector[to] = inward_vector[to] + 1
+#   outward_vector[from] = outward_vector[from] + 1
+# }
 
-calculate_inward_outward = function(ids) {
+calculate_inward_outward = function(ids,file_path = 'Project/data/allcites.txt') {
   inward_vector = rep(0, length(ids))
   outward_vector = rep(0, length(ids))
-  lines = readLines('Project/data/allcites.txt')
+  lines = readLines(file_path)
   #We switched to the for loop to avoid the copy/re-instanciation of the vectors, so we can update them directly
   for (line_index in 1:length(lines)) {
     line = lines[line_index]
@@ -31,8 +31,9 @@ calculate_inward_outward = function(ids) {
     inward_vector[to] = inward_vector[to] + 1
     outward_vector[from] = outward_vector[from] + 1
   }
-  inward_vector
-  outward_vector
+  return (list('inward_vector'=inward_vector,'outward_vector'=outward_vector))
+#   inward_vector
+#   outward_vector
 }
 
-calculate_inward_outward(ids)
+# calculate_inward_outward(ids)
--- Project/tests/inward_outward.tests.r
+++ Project/tests/inward_outward.tests.r
@@ -4,6 +4,10 @@ library(testthat)
 ids = c(1, 2, 3, 4)
 
 test_that('', {
-  inward = calculate_inward_outward(ids, 'Project/tests/small_sample_1/allcities.txt')
-  expect_equal(inward,c(3,0,0,0))
+  io = calculate_inward_outward(ids, 'Project/tests/small_sample_1/allcities.txt')
+  inward = io$inward_vector
+  outward = io$outward_vector
+
+  expect_equal(inward, c(0, 1, 1, 1))
+  expect_equal(outward, c(3, 0, 0, 0))
 })
