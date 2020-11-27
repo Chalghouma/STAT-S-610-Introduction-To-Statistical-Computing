5a833a704d231aeccdd439810547c0aa363a742f
Cherif Redissi <cherifredissi@hotmail.com>
2020-11-27 18:01:11 -0500

    feat: [Project] calculate inward & outward vectors

--- Project/selector.r
+++ Project/selector.r
@@ -4,8 +4,35 @@ library(tidyverse)
 
 cases = read_csv('Project/data/judicial.csv')
 my_data <- as_tibble(cases)
+ids = my_data[, 1][[1]]
 cases_in_range = function(df, from, to) {
   ids_and_years = my_data %>% select(1, 4)
-  return (ids_and_years %>% filter(year > 2754))
+  return(ids_and_years %>% filter(year > 2754))
 }
 
+process_line = function(line, inward_vector, outward_vector) {
+  splitted = str_split(line, ' ')[[1]]
+  from = splitted[1] %>% strtoi()
+  to = splitted[2] %>% strtoi()
+  inward_vector[to] = inward_vector[to] + 1
+  outward_vector[from] = outward_vector[from] + 1
+}
+
+calculate_inward_outward = function(ids) {
+  inward_vector = rep(0, length(ids))
+  outward_vector = rep(0, length(ids))
+  lines = readLines('Project/data/allcites.txt')
+  #We switched to the for loop to avoid the copy/re-instanciation of the vectors, so we can update them directly
+  for (line_index in 1:length(lines)) {
+    line = lines[line_index]
+    splitted = str_split(line, ' ')[[1]]
+    from = splitted[1] %>% strtoi()
+    to = splitted[2] %>% strtoi()
+    inward_vector[to] = inward_vector[to] + 1
+    outward_vector[from] = outward_vector[from] + 1
+  }
+  inward_vector
+  outward_vector
+}
+
+calculate_inward_outward(ids)
