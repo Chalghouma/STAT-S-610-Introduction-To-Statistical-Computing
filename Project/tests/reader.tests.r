10e9067785c4a43cc8802bd5d9cc18ab59f579ac
Cherif Redissi <cherifredissi@hotmail.com>
2020-11-28 10:25:48 -0500

    chore: [Project] add NA test for separate years

--- Project/tests/reader.tests.r
+++ Project/tests/reader.tests.r
@@ -14,10 +14,25 @@ test_that('Should have 30288ids', {
 })
 
 test_that('Should throw a warning', {
-  expect_warning(function() {
+  expect_warning({
     get_auth_by_year(auth_df, 1799)
   })
-  expect_warning(function() {
+  expect_warning({
     get_auth_by_year(auth_df, 2003)
   })
+})
+
+
+test_that('Should match authorities for year 2002', {
+  auth_value_case_id_5 = 2.913e-05
+  auth_value_case_id_30288 = 0
+  authority_vector = get_auth_by_year(auth_df, 2002)
+  expect_equal(auth_value_case_id_5, authority_vector[5])
+  expect_equal(auth_value_case_id_30288, authority_vector[30288])
+})
+
+test_that('Should match authorities for year 2001', {
+  auth_value_case_id_30288 = NA
+  authority_vector = get_auth_by_year(auth_df, 2001)
+  expect_equal(auth_value_case_id_30288, authority_vector[30288])
 })
