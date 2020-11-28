691ec79db71f3cd8fc774803075611fdc24dde60
Cherif Redissi <cherifredissi@hotmail.com>
2020-11-28 10:06:26 -0500

    chore: [Project] add auth column reading tests

--- Project/tests/reader.tests.r
+++ Project/tests/reader.tests.r
@@ -6,9 +6,18 @@ library(readr)
 auth_df = read_auth()
 
 test_that('Should have 203years + 1 column', {
-    expect_that(length(auth_df),204)
+  expect_equal(length(auth_df), 204)
 })
 
-test_that('Should have 30288ids',{
-    expect_that(length(auth_df[,1]),30288)
+test_that('Should have 30288ids', {
+  expect_equal(length(auth_df[, 1]), 30288)
+})
+
+test_that('Should throw a warning', {
+  expect_warning(function() {
+    get_auth_by_year(auth_df, 1799)
+  })
+  expect_warning(function() {
+    get_auth_by_year(auth_df, 2003)
+  })
 })
