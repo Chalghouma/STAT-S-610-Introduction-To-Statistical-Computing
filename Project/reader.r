c456361734a6398aeb3522750f3129b0b61dbeed
Cherif Redissi <cherifredissi@hotmail.com>
2020-11-28 10:07:10 -0500

    feat: [Project] get authority vector by year

--- Project/reader.r
+++ Project/reader.r
@@ -4,3 +4,10 @@ library(readr)
 read_auth = function(path = 'Project/data/authmat.txt'){
     df = read.csv(path)
 }
+
+get_auth_by_year = function(auth_df, year){
+    #We already know the first year is 1800
+    if(year < 1800 | year > 2002)
+        return(warning('Year should be between 1800 and 2002'))
+    return (auth_df[, (year - 1800) + 1 + 1 ])
+}
