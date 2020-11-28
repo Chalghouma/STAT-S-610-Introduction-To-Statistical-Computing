6382ff26d99ee8a603498f6b32acb0c90af4b6fb
Cherif Redissi <cherifredissi@hotmail.com>
2020-11-28 10:50:29 -0500

    refactor: [Project] update df.selections methods

--- Project/selector.r
+++ Project/selector.r
@@ -1,39 +1,36 @@
 # install.packages('tidyverse')
-library(readr)
 library(tidyverse)
 
-# cases = read_csv('Project/data/judicial.csv')
-# my_data <- as_tibble(cases)
-# ids = my_data[, 1][[1]]
-# cases_in_range = function(df, from, to) {
-#   ids_and_years = my_data %>% select(1, 4)
-#   return(ids_and_years %>% filter(year > 2754))
-# }
+cases_in_range = function(df, from, to) {
+  return(df %>% filter(year >= from & year <= to))
+}
+cases_until = function(df, until) {
+  first_year = df[1,]$year
+  return (cases_in_range(df,first_year,until))
+}
 
-# process_line = function(line, inward_vector, outward_vector) {
-#   splitted = str_split(line, ' ')[[1]]
-#   from = splitted[1] %>% strtoi()
-#   to = splitted[2] %>% strtoi()
-#   inward_vector[to] = inward_vector[to] + 1
-#   outward_vector[from] = outward_vector[from] + 1
-# }
+parse_line = function(line) {
+  splitted = str_split(line, ' ')[[1]]
+  citing = splitted[1] %>% strtoi()
+  cited = splitted[2] %>% strtoi()
 
-calculate_inward_outward = function(ids,file_path = 'Project/data/allcites.txt') {
+  return(list('citing'=citing,'cited'=cited))
+}
+
+calculate_inward_outward = function(ids, file_path = 'Project/data/allcites.txt') {
   inward_vector = rep(0, length(ids))
   outward_vector = rep(0, length(ids))
   lines = readLines(file_path)
   #We switched to the for loop to avoid the copy/re-instanciation of the vectors, so we can update them directly
   for (line_index in 1:length(lines)) {
+    parsed = parse_line(lines[line_index])
     line = lines[line_index]
     splitted = str_split(line, ' ')[[1]]
-    from = splitted[1] %>% strtoi()
-    to = splitted[2] %>% strtoi()
-    inward_vector[to] = inward_vector[to] + 1
-    outward_vector[from] = outward_vector[from] + 1
+    citing = splitted[1] %>% strtoi()
+    cited = splitted[2] %>% strtoi()
+    
+    inward_vector[cited] = inward_vector[cited] + 1
+    outward_vector[citing] = outward_vector[citing] + 1
   }
-  return (list('inward_vector'=inward_vector,'outward_vector'=outward_vector))
-#   inward_vector
-#   outward_vector
+  return(list('inward_vector' = inward_vector, 'outward_vector' = outward_vector))
 }
-
-# calculate_inward_outward(ids)
