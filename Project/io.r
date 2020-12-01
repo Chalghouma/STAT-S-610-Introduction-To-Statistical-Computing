9cd81da5b32329fa42f7ee468c2a3aa7aa2b750c
Cherif Redissi <cherifredissi@hotmail.com>
2020-12-01 13:06:33 -0500

    feat: [Project] provide functions to write authorities/hubs

---
+++ Project/io.r
@@ -0,0 +1,15 @@
+library(readr)
+
+write_vector = function(data, year, category ) {
+  path = paste('Project/data/', category, '/', year, '.csv', sep = '')
+  df = data.frame(matrix(ncol = 1, nrow = length(vector), dimnames = list(NULL, c(category))))
+  df[category] = vector
+  write.csv(df, path)
+}
+
+write_authorities = function(data, year) {
+  write_vector(data, year, category = 'authority')
+}
+write_hubs = function(data, year) {
+  write_vector(data, year, category = 'hubs')
+}
