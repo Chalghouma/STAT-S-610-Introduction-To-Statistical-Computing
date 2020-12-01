c40cedbf1f377f6a2e0d50e8029fda390d0777de
Cherif Redissi <cherifredissi@hotmail.com>
2020-12-01 13:46:42 -0500

    feat: [Project] read/write authorities/hubs

--- Project/io.r
+++ Project/io.r
@@ -1,9 +1,9 @@
 library(readr)
 
-write_vector = function(data, year, category ) {
+write_vector = function(data, year, category) {
   path = paste('Project/data/', category, '/', year, '.csv', sep = '')
-  df = data.frame(matrix(ncol = 1, nrow = length(vector), dimnames = list(NULL, c(category))))
-  df[category] = vector
+  df = data.frame(matrix(ncol = 1, nrow = length(data), dimnames = list(NULL, c(category))))
+  df[category] = data
   write.csv(df, path)
 }
 
@@ -11,13 +11,19 @@ write_authorities = function(data, year) {
   write_vector(data, year, category = 'authority')
 }
 write_hubs = function(data, year) {
-  write_vector(data, year, category = 'hubs')
+  write_vector(data, year, category = 'hub')
 }
 
-
-read_authorities = function(year){
-  
+read_vector = function(year, category) {
+  path = paste('Project/data/', category, '/', year, '.csv', sep = '')
+  df = read_csv(path)
+  return(df[, 2][[1]])
 }
-read_hubs = function(year){
-  
+
+read_authorities = function(year) {
+  return(read_vector(year, category = 'authority'))
+}
+
+read_hubs = function(year) {
+  return(read_vector(year, category = 'hub'))
 }
