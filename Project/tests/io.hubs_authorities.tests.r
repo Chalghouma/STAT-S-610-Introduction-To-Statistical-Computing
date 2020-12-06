7409077613794ae01589ae470734b4032c0f15c9
Cherif Redissi <cherifredissi@hotmail.com>
2020-12-06 15:03:16 -0500

    chore: [Project] add tests for authorities' reading

--- Project/tests/io.hubs_authorities.tests.r
+++ Project/tests/io.hubs_authorities.tests.r
@@ -1,4 +1,5 @@
-source('Project/io/hubs_authorities.r')
+source('Project/source_paths.r')
+source(get_hubs_authorities_io_file_path())
 library(testthat)
 
 
@@ -20,5 +21,12 @@ test_that("Hardcoded hubs' written vector should be the same", {
   expect_equal(written_data, hubs)
 })
 
+test_that('',{
+  df = read_authorities_df_from_year_interval(2000:2000)
+  expect_equal(get_authority_from_generated_authorities_df(df,1, 2000) , 0.25)
+  expect_equal(get_authority_from_generated_authorities_df(df,2, 2000) , 0.1)
+  expect_equal(get_authority_from_generated_authorities_df(df,3, 2000) , 0.3)
+  expect_equal(get_authority_from_generated_authorities_df(df,4, 2000) , 0.07)
+})
 
 
