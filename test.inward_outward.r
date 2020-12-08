014a00a7c3cbe954a35c494ad374f74a636ad55a
Cherif Redissi <cherifredissi@hotmail.com>
2020-12-08 06:39:48 -0500

    refactor: [Project] relocate sample_1 test data

--- test.inward_outward.r
+++ test.inward_outward.r
@@ -7,10 +7,10 @@ library(tidyverse)
 
 
 test_that('Should have the exact same inward/outward', {
-  ids = c(1, 2, 3, 4)
-  io = calculate_inward_outward(ids, './Project/tests/small_sample_1/allcities.txt')
-  inward = io$inward_vector
-  outward = io$outward_vector
+  ids <- c(1, 2, 3, 4)
+  io <- calculate_inward_outward(ids, './Project/data/tests/small_sample_1/allcities.txt')
+  inward <- io$inward_vector
+  outward <- io$outward_vector
 
   expect_equal(inward, c(0, 1, 1, 1))
   expect_equal(outward, c(3, 0, 0, 0))
@@ -21,16 +21,17 @@ test_that('Should have the exact same inward/outward', {
 test_that('Is supposed to have the same inward/outward values. (But fails due to the .csv inconsistency', {
   cases = read_csv('Project/data/judicial.csv')
   my_data <- as_tibble(cases)
-  projected = my_data %>% select(1, 9, 10)
-  ids = projected[,1]$caseid
+  
+  case_ids_with_inout_deg <- my_data %>% select(1, 9, 10)
+  ids <- case_ids_with_inout_deg[,1]$caseid
 
-  ino = calculate_inward_outward(ids)
-  inward =  ino$inward_vector
-  outward = ino$outward_vector
+  ino <- calculate_inward_outward(ids)
+  inward <-  ino$inward_vector
+  outward <- ino$outward_vector
 
 
-  indeg = projected[, 2]$indeg
-  outdeg = projected[, 3]$outdeg
+  indeg <- case_ids_with_inout_deg[, 2]$indeg
+  outdeg <- case_ids_with_inout_deg[, 3]$outdeg
   expect_equal(inward, indeg)
   expect_equal(outward, outdeg)
 })
